db927c898fe97e75cf9531156a03ec4b5c40782f
Malek Alarmash <81653238+Malekalarmash@users.noreply.github.com>
2023-10-30 04:36:45 -0400

    configuring Github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/project-planner-cilent-/",
+  base: "/project-planner-cilent/",
   plugins: [react()],
 });
